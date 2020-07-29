82509b2c36788ed496953fdf890563f340954a1d
jalilmanuel25 <jalilmanuel25@gmail.com>
2020-07-29 18:10:11 -0500

    Versión original

---
+++ conectar_utd.php
@@ -0,0 +1,11 @@
+<?php
+   //definir la variables de conexion
+   
+   $servidor="localhost";
+   $bd="utd";
+   $user="root";
+   $pass="Jml14";
+
+  $conexion=mysqli_connect($servidor,$user,$pass,$bd);
+
+?>
